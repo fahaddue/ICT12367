c29edb1237d2e08fd4a62ca2e1b6bf0c22d8b05c
fahaddue <fahad.due@spumail.net>
2026-03-28 22:17:57 +0700

    Delete Lab11/company_sys directory

--- Lab11/company_sys/services/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from services.models import Person
-
-# Register your models here.
-admin.site.register(Person)
--- Lab11/company_sys/services/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.index),
-    path('index', views.index),
-    path('products', views.products),
-    path('feedback', views.feedback),
-]
